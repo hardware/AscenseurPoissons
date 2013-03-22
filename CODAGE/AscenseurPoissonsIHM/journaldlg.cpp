b6a3ed9cf46977a6b1e84176393d70723ad1855d
Quentin Panissier <quentin.panissier@gmail.com>
2013-03-22 15:44:42 +0100

    Ajout d'un méthode de lecture du fichier de logs

--- CODAGE/AscenseurPoissonsIHM/journaldlg.cpp
+++ CODAGE/AscenseurPoissonsIHM/journaldlg.cpp
@@ -2,14 +2,9 @@
 
 JournalDlg::JournalDlg(QWidget *parent, Seance *ptSeance) : QWidget(parent), pSeance(ptSeance)
 {
-    QListWidgetItem *item = new QListWidgetItem(QString::fromUtf8("Jan 28 12:21:37 [DEFAUT] - Un défaut est survenu au niveau de la pompe FLYGT"));
-    item->setForeground(QBrush(QColor("red")));
-    item->setToolTip("DEFAUT");
-
     LWLogs = new QListWidget;
     LWLogs->setFont(QFont("Arial", 10));
-    LWLogs->addItem(QString::fromUtf8("Jan 28 12:21:37 [INFO] - Une séance vient d'être paramétrée"));
-    LWLogs->addItem(item);
+    LWLogs->addItems(LireFichierLog());
 
     VLMain = new QVBoxLayout(this);
     VLMain->addWidget(LWLogs);
@@ -21,9 +16,24 @@ JournalDlg::JournalDlg(QWidget *parent, Seance *ptSeance) : QWidget(parent), pSe
 
 void JournalDlg::afficherLog(QListWidgetItem *item)
 {
-    if(item->toolTip() == "DEFAUT")
-        QMessageBox::critical(this, item->toolTip(), item->text());
-    else
-        QMessageBox::information(this, item->toolTip(), item->text());
+    QMessageBox::information(this, "INFORMATION", item->text());
+}
 
+QStringList JournalDlg::LireFichierLog()
+{
+    QString ligne;
+    QFile file(QApplication::applicationDirPath() + "/logs.txt");
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file);
+
+        while ( ! in.atEnd() )
+        {
+            ligne = in.readLine();
+            listeLignes.push_back(ligne);
+        }
+    }
+
+    return listeLignes;
 }
--- CODAGE/AscenseurPoissonsIHM/journaldlg.h
+++ CODAGE/AscenseurPoissonsIHM/journaldlg.h
@@ -7,6 +7,9 @@
 #include <QVBoxLayout>
 #include <QListWidget>
 #include <QMessageBox>
+#include <QFile>
+#include <QApplication>
+#include <QTextStream>
 
 class JournalDlg : public QWidget
 {
@@ -18,11 +21,14 @@ public:
     explicit JournalDlg(QWidget *parent = 0, Seance *ptSeance = 0);
     
 private:
+    QStringList listeLignes;
+
     QVBoxLayout *VLMain;
     QListWidget *LWLogs;
 
 public slots:
     void afficherLog(QListWidgetItem *item);
+    QStringList LireFichierLog();
 
 signals:
     
